2a9f618c04ab9df241d7b0938a3bd5809d8be5c6
Veronica Kim <vero.a.kim@gmail.com>
2017-12-26 15:43:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,12 +14,12 @@ class TicTacToe
         [2,4,6]
       ]
 
-      def display_board(@board)
-        puts " #{[0]} | #{[1]} | #{[2]} "
+      def display_board@board
+        puts " #{board[0]} | #{board[1]} | #{board[2]} "
         puts "-----------"
-        puts " #{[3]} | #{[4]} | #{[5]} "
+        puts " #{board[3]} | #{board[4]} | #{board[5]} "
         puts "-----------"
-        puts " #{[6]} | #{[7]} | #{[8]} "
+        puts " #{board[6]} | #{board[7]} | #{board[8]} "
       end
 
 end
