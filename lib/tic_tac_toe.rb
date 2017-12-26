80d5f35bcec3f614398346a3a92855264bc37017
Veronica Kim <vero.a.kim@gmail.com>
2017-12-26 15:49:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,5 +34,7 @@ class TicTacToe
         @board[location] != " " && @board[location] != ""
         end
 
-
+        def valid_move?(board, index)
+          index.between?(0,8) && !position_taken?(board, index)
+        end
 end
