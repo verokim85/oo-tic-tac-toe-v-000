ba5e361435590019fb951069e28fa2dc014b6278
Veronica Kim <vero.a.kim@gmail.com>
2017-12-26 15:55:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,6 +58,19 @@ class TicTacToe
         end
 
 
+                def turn
+                  puts "Please enter 1-9:"
+                  input = gets.strip
+                  index = input_to_index(input)
+                  token = current_player
+                  if valid_move?(index)
+                    move(index, token)
+                    display_board
+                  else
+                    turn
+                  end
+                end
+
 
 
 end
